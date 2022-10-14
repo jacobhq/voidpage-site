d0a348a0a2317250ec3d91be36db44347bb527ee
Jacob Marshall <jacob@omg.lol>
2022-10-14 16:45:04 +0000

    🔍️ Title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Footer from 'components/Footer'
 import Hero from 'components/Hero'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Voidpage - Bringing a beautiful newtab to the masses</title>
+      </Head>
       <Hero />
       <Footer />
     </>
